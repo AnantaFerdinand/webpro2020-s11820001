bbfefb033885234460bb3bdccfab12e1106890cc
AnantaFerdinand <s11820001@student.unklab.ac.id>
2020-11-09 10:27:03 +0800

    15th commit

---
+++ pertemuan_15/hello.js
@@ -0,0 +1,16 @@
+
+// export const name ='Jane';
+
+// export const numbers = [1,2,3,4,5];
+
+import {world} from './components/world.js'
+
+export const hello = (user) =>{
+  world();
+  console.log(`Hello, ${user}`);
+}
+
+export default hello;
+
+
+
---
+++ pertemuan_15/index.js
@@ -0,0 +1,24 @@
+// import {hello, world, numbers} from './hello.js'
+
+// hello('John Doe');
+// world('Hello World');
+// console.log(name);
+// console.log(numbers);
+
+// import * as helper from './hello.js'
+
+// helper.hello('John Doe');
+// helper.world('Hello World');
+// console.log(helper.name);
+// console.log(helper.numbers);
+
+// import hello from './hello.js'
+// hello('John Doe');
+
+
+import Func1 from './hello.js'
+Func1('John Doe');
+
+
+
+
